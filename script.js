50606349fa452ec121e174b88a5441555a25230a
Cristian Henrique <chparatrabalho@gmail.com>
2024-01-27 20:16:14 -0300

    att

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
+
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
